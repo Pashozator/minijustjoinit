d7479acabe8a34065b4b12e7eca8c8c254332f0d
Wojciech Szućko <wojciech.szucko@gmail.com>
2021-08-24 22:36:13 +0200

    fix: change grid and offers list item

--- src/modules/offers/components/OffersListItem/OffersListItem.tsx
+++ src/modules/offers/components/OffersListItem/OffersListItem.tsx
@@ -11,11 +11,11 @@ export const OffersListItem: React.FC<OffersListItemProps> = props => {
 				<img className={styles.logo} src={offer.company_logo_url} alt=""/>
 			</div>
 			<div className={styles.info}>
-				<div className={styles.title}>{offer.title} [{offer.salary_from} - {offer.salary_to} {offer.salary_currency}]</div>
+				<div className={styles.title}>{offer.title}</div>
 				<div className={styles.additional}>
-					<span>Firma: {offer.company_name}</span>
-					<span>Miasto: {offer.city}</span>
-					<span>Typ zatrudnienia: {offer.employment_type}</span>
+					<span>{offer.company_name}</span>
+					<span>{offer.city}</span>
+					<span>{offer.salary_from} - {offer.salary_to} {offer.salary_currency}</span>
 				</div>
 			</div>
 		</div>
