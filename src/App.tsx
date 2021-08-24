6315dcfe1ac121ffe58348119dbdd05c8306967d
Wojciech Szućko <wojciech.szucko@gmail.com>
2021-08-24 19:55:16 +0200

    feat: add home page view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HomePageView } from './views/HomePageView/HomePageView';
 
 function App() {
-  return (
-    <div></div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" component={HomePageView}/>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/views/HomePageView/HomePageView.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const HomePageView: React.FC = () => {
+	return (
+		<div>home page view</div>
+	);
+}
