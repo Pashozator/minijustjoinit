f857f394bc7bb19ac58fb6e1f64360932ccfced7
Wojciech Szućko <wojciech.szucko@gmail.com>
2021-08-24 20:53:42 +0200

    feat: add basic grid

--- src/views/HomePageView/HomePageView.tsx
+++ src/views/HomePageView/HomePageView.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import styles from './HomePageView.module.css';
 
 export const HomePageView: React.FC = () => {
 	return (
-		<div>home page view</div>
+		<div className={styles.container}>
+			<div>list</div>
+			<div>map</div>
+		</div>
 	);
 }
